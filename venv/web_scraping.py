d3b01a2bd569b96ef20a1411b7046ad4ad1333c0
hernancedillo <hcedill1@asu.edu>
2023-03-08 10:11:52 -0600

    libraries for scraping to run properly

--- venv/web_scraping.py
+++ venv/web_scraping.py
@@ -1,1 +1,25 @@
-print('hello')
+import pandas as pd
+import credentials as cred
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+def runWithoutClosing():
+    while(True):
+        pass
+
+def main():
+
+    driver = webdriver.Chrome(
+        executable_path='/Users/hernancedillo/Documents/GitHubLocalRepo/WebScrapingGrades/venv/chromeDriver/chromedriver')
+    driver.get('https://oxylabs.io/blog')
+    usernameCred = cred.username
+    passwordCred = cred.password
+    runWithoutClosing()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
